cb6bcc2356d4b300813556da708bd37155ff2fd0
Wafaeyy <youssefmohamedwafaey@gmail.com>
2025-04-16 21:16:57 +0200

    fix errors in Event class

--- EventManagementSystem/src/main/java/project/eventmanagementsystem/Event.java
+++ EventManagementSystem/src/main/java/project/eventmanagementsystem/Event.java
@@ -17,19 +17,19 @@ import java.util.Objects;
 public class Event {
     private String name;
     private String description;
-    private String category;
+    private Category category;
     private long price;
-    private int roomId;
+    private Room room;
     private Organizer organizer;
     private final List<Attendee> attendees = new ArrayList<>();
     private Date date;
 
-    public Event(String name, String description, String category, long price, int roomId, Date date) {
+    public Event(String name, String description, Category category, long price, Room room, Date date) {
         this.name = name;
         this.description = description;
         this.category = category;
         this.price = price;
-        this.roomId = roomId;
+        this.room = room;
         this.date = date;
     }
 
@@ -97,11 +97,12 @@ public class Event {
         return attendees.remove(attendee);
     }
 
-    public LocalDateTime getDate() {
-        return date;
+    public Date getDate() {
+        Date def_copy_date = new Date(date.getTime());
+        return def_copy_date;
     }
 
-    public void setDate(LocalDateTime date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
